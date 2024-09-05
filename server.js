c3f4a25c8685e1034c698e7df0b9fdba8f6a49dc
yezdanibegum <yezdanibegum@gmail.com>
2024-09-05 13:37:00 +0100

    Updated configuration and resolved deprecation warnings

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const attendeesRouter = require('./routes/attendees');
-const usersRouter = require('./routes/users');
-const timesheetsRouter = require('./routes/timesheets');
+   const express = require('express');
+   const mongoose = require('mongoose');
+   const cors = require('cors');
+   const attendeesRouter = require('./routes/attendees');
+   const usersRouter = require('./routes/users');
+   const timesheetsRouter = require('./routes/timesheets');
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+   const app = express();
+   app.use(cors());
+   app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+   mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to Database'));
+   const db = mongoose.connection;
+   db.on('error', (error) => console.error(error));
+   db.once('open', () => console.log('Connected to Database'));
 
-app.use('/attendees', attendeesRouter);
-app.use('/users', usersRouter);
-app.use('/timesheets', timesheetsRouter);
+   app.use('/attendees', attendeesRouter);
+   app.use('/users', usersRouter);
+   app.use('/timesheets', timesheetsRouter);
 
-app.listen(3000, () => console.log('Server Started'));
+   const PORT = process.env.PORT || 3000;
+   app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
